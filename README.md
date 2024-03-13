59ade911d14a8207aedc2528d3417123acc351b9
HeinDuijf <duijf.hein@gmail.com>
2024-03-13 23:54:22 +0100

    created visualization and updated readme

--- README.md
+++ README.md
@@ -1,22 +1,26 @@
-# Opinion Dynamics: The Hegselman-Krause model
+# Opinion Dynamics: The Bounded Confidence Model
 
-This is a Python project on the Hegselmann-Krause model of opinion dynamics. 
+This is a repository on the bounded confidence model of opinion dynamics, as first put forward by [Hegselmann & Krause](https://www.jasss.org/5/3/2.html). This repository contains code for the model and for some visualizations. 
+
+[![A picture of an example of a run of a bounded confidence model](/images/example.png  "An example of a bounded confidence model")]()
+
 ## 1. Setup
 To run the project, you first need to install the required packages
 ```commandline
 pip install -r requirements.txt
 ```
 
-## 2. The model
+## 2. The model `model.py`
+The class `BoundedConfidence` in the module `model.py` implements the bounded confidence model. 
+
+## 3. Visualization `visualize.py`
+The method `visualize` in the module `visualize.py` can be used to visualize the run of a bounded confidence model. 
 
 
-## 3. Organization of the project
-
-
-## X. Licence and citation
-This repository accompanies an academic paper (under review). In the meantime, 
+## A. Licence and citation
+This repository accompanies ongoing research. In the meantime, 
 please cite as follows:
 
-Cite the code: 
+- Duijf, H. (2024). An implementation and visualization of the bounded confidence model. https://github.com/HeinDuijf/OpinionDynamicsHK
 
 Released under the [MIT licence](LICENCE.md).
---
+++ visualize.py
@@ -0,0 +1,52 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import networkx as nx
+
+from model import BoundedConfidence
+
+
+def visualize(model: BoundedConfidence, steps: int, digits: int = 2):
+    # get model run results
+    results = model.run(number_of_steps=steps)
+    results.drop_duplicates(inplace=True)
+    steps = len(results.index)
+
+    # initialize
+    net = nx.DiGraph()
+    norm = mpl.colors.Normalize(
+        vmin=min(model.start_distribution),
+        vmax=max(model.start_distribution),
+    )
+    colormap = plt.get_cmap("cool")  # other options: rainbow
+
+    # add nodes
+    for step in range(steps):
+        opinions = results.loc[step, :].drop_duplicates()
+        for opinion in opinions:
+            net.add_node(
+                (opinion, step),
+                pos=(step, opinion),
+                color=mpl.colors.to_hex(colormap(norm(opinion))),
+            )
+    pos = nx.get_node_attributes(net, "pos")
+    colors = list(nx.get_node_attributes(net, "color").values())
+    labels = {node: round(node[0], 2) for node in net.nodes()}
+
+    # add edges
+    for step in range(steps - 1):
+        for agent in model.agents:
+            source_node = (results.at[step, agent], step)
+            target_node = (results.at[step + 1, agent], step + 1)
+            edge = (source_node, target_node)
+            if edge not in net.edges():
+                net.add_edge(*edge)
+
+    # draw
+    options = {"node_size": 1000, "font_family": "SegUI", "font_size": 12}
+    nx.draw(net, pos=pos, node_color=colors, **options)
+    nx.draw_networkx_labels(net, pos, labels)
+
+
+start = [0.1, 0.2, 0.3, 0.35]
+model = BoundedConfidence(start, confidence_threshold=0.1)
+visualize(model, 5)
